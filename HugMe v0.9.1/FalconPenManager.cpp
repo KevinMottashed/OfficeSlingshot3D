0ea90852af827755f20ce99bfa106f0a8584ddca
Alexandre Bradley <lopert@gmail.com>
2010-03-24 21:25:24 -0400

    Created the control loop. (Issue #25) Generates a random local falcon position. (Issue #26) Generates a random local projectile and its speed. (Issue #27)
    Signed-off-by: Alexandre Bradley <lopert@gmail.com>

--- HugMe v0.9.1/FalconPenManager.cpp
+++ HugMe v0.9.1/FalconPenManager.cpp
@@ -11,9 +11,73 @@
 
 FalconPenManager::FalconPenManager()
 {
+	falcon_enabled = false;
 }
 
 FalconPenManager::~FalconPenManager()
 {
 
 }
+
+DWORD FalconPenManager::getPositionFromFalcon(FalconPenManager* p_Falcon)
+{
+	double x,y,z;
+	//cVector3d position;
+	
+
+	while(p_Falcon->falcon_enabled) 
+	{
+		x = rand() % 10;
+		y = rand() % 10;
+		z = rand() % 10;
+		
+		cVector3d position;
+
+		position.x = x;
+		position.y = y;
+		position.z = z;
+
+		p_Falcon->m_pController->notifyNewLocalSlingshotPosition(position);
+
+		cVector3d new_projectile;
+		cVector3d new_speed;
+
+		new_projectile.x = x;
+		new_projectile.y = y;
+		new_projectile.z = z;
+
+		new_speed.x = x;
+		new_speed.y = y;
+		new_speed.z = z;
+
+		p_Falcon->m_pController->notifyNewLocalProjectile(new_projectile, new_speed);
+
+	}
+
+	return 0;
+
+}
+
+// TODO implement
+// start managing the falcon pen (polling it for information)
+// create a thread to poll it
+void FalconPenManager::start() 
+{
+	falcon_enabled = true;
+
+	// Thread Generation, modified from ZCameraManager.cpp
+	DWORD threadId;
+	HANDLE hThread = CreateThread( 0, 0,(LPTHREAD_START_ROUTINE) getPositionFromFalcon,  (void*) this, 0, &threadId);
+
+}
+
+// TODO implement
+// stop managing the falcon pen
+// stop the thread that's polling it
+void FalconPenManager::stop() 
+{
+	falcon_enabled = false;
+
+	// Kill thread here
+}
+
--- HugMe v0.9.1/FalconPenManager.h
+++ HugMe v0.9.1/FalconPenManager.h
@@ -23,16 +23,21 @@ public:
 	// TODO implement
 	// start managing the falcon pen (polling it for information)
 	// create a thread to poll it
-	void start() {}
-
+	void start();
 	// TODO implement
 	// stop managing the falcon pen
 	// stop the thread that's polling it
-	void stop() {}
+	void stop();
+
+	// Creates a random 3dVector
+	static DWORD getPositionFromFalcon(FalconPenManager* p_Falcon);
 
 private:
 	FalconPenManager(const FalconPenManager& falconPenManager); // intentionally not implemented
 	FalconPenManager& operator=(const FalconPenManager& falconPenManager); // intentionally not implemented
+
+	bool falcon_enabled;
 };
 
 #endif // !defined(AFX_FALCONPENMANAGER_H__AFCF2776_E117_47EC_94C7_97838C3AFB59__INCLUDED_)
+
