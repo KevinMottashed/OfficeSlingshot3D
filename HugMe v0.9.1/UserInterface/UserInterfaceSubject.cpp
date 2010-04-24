2ea30f6081b8019f55331ed3261fc1e96116cecd
Kevin Mottashed <kevinmottashed@gmail.com>
2010-04-24 13:45:22 -0400

    Code cleanup
    Removed some useless code that I was using to help me debug.

--- HugMe v0.9.1/UserInterface/UserInterfaceSubject.cpp
+++ HugMe v0.9.1/UserInterface/UserInterfaceSubject.cpp
@@ -14,7 +14,6 @@ void UserInterfaceSubject::attach(UserInterfaceObserver* observer)
 	assert(std::find(m_observers.begin(), m_observers.end(), observer) == m_observers.end());
 
 	m_observers.push_back(observer);
-	unsigned int size = m_observers.size();
 	return;
 }
 
@@ -27,7 +26,6 @@ void UserInterfaceSubject::detach(UserInterfaceObserver* observer)
 void UserInterfaceSubject::notify(UserInterfaceUpdateContext context, const void* data)
 {
 	std::vector<UserInterfaceObserver*>::iterator it;
-	unsigned int size = m_observers.size();
 	for (it = m_observers.begin(); it != m_observers.end(); ++it)
 	{
 		(*it)->update(context, data);
