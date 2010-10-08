dc174f87cf544f3fabae14aabbfdf4b2ec66e8b2
Kevin Mottashed <kevinmottashed@gmail.com>
2010-10-07 20:25:35 -0400

    Added assert_equals for floats/doubles in the test framework.

--- HugMe v0.9.1/Test/Framework/UnitTestAsserts.cpp
+++ HugMe v0.9.1/Test/Framework/UnitTestAsserts.cpp
@@ -14,3 +14,25 @@ void assert_false(bool a, const std::string& message)
 	assert_true(!a, message);
 	return;
 }
+
+void assert_equals(const double& actual, const double& expected, const std::string& message, float error_margin)
+{
+	bool fail = false;
+	if (actual > expected)
+	{
+		fail = actual - expected > error_margin;
+	}
+	else
+	{
+		fail = expected - actual > error_margin;
+	}
+
+	if (fail)
+	{
+		std::ostringstream os;
+		os << "Actual=" << actual << " Expected=" << expected << "Error Margin=" << error_margin << " - " << message;
+		throw TestFailedException(os.str());
+	}
+
+	return;
+}
--- HugMe v0.9.1/Test/Framework/UnitTestAsserts.h
+++ HugMe v0.9.1/Test/Framework/UnitTestAsserts.h
@@ -5,9 +5,25 @@
 
 #include "TestFailedException.h"
 
-// throws a test failed exception if a != b
+/**
+ * Throws a test failed exception if a != b.
+ * @param actual The value that was received
+ * @param expected The value that was expected
+ * @param message The message to display if the test fails
+ */
 template <typename S, typename T>
 void assert_equals(const S& actual, const T& expected, const std::string& message = "");
+
+/**
+ * Throws a test failed exception if a != b.
+ * This version is used for double and floats. 
+ * A small margin of error is allowed due to the whole floating point comparison thing.
+ * @param actual The value that was received
+ * @param expected The value that was expected
+ * @param message The message to display if the test fails
+ * @param error_margin The error margin to use when comparing the 
+ */
+void assert_equals(const double& actual, const double& expected, const std::string& message = "", float error_margin = 0.001f);
 
 // throws a test failed exception if a is false
 void assert_true(bool a, const std::string& message = "");
@@ -22,7 +38,8 @@ void assert_false(bool a, const std::string& message = "");
 template <typename S, typename T>
 void assert_equals(const S& actual, const T& expected, const std::string& message)
 {
-	if (actual != expected)
+	// some classes don't define the != operator so use the == operator instead
+	if (!(actual == expected))
 	{
 		std::ostringstream os;
 		os << "Actual=" << actual << " Expected=" << expected << " - " << message;
