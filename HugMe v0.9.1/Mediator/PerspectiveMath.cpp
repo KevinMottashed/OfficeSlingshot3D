180b0dcbf7e4372bd03335cd9cd4cbcd647ca30d
Kevin Mottashed <kevinmottashed@gmail.com>
2010-10-07 21:03:48 -0400

    Fix for convertBoxToBox (issue #195)
    Closes #195

--- HugMe v0.9.1/Mediator/PerspectiveMath.cpp
+++ HugMe v0.9.1/Mediator/PerspectiveMath.cpp
@@ -24,18 +24,22 @@ void PerspectiveMath::invert3DProjectile(Projectile& projectile)
 	projectile.force(adjustedForce);
 }
 
-void PerspectiveMath::convertOrientationXYZtoYZX(cVector3d& vector) {
+void PerspectiveMath::convertOrientationXYZtoYZX(cVector3d& vector) 
+{
 	double temp = vector.x;
 	vector.x = vector.y;
 	vector.y = vector.z;
 	vector.z = temp;
 }
 
-void PerspectiveMath::convertBoxToBox(cVector3d& vector, const cCollisionAABBBox& boxOrigin, const cCollisionAABBBox& boxTarget) {
-	vector.x = vector.x / (boxOrigin.getUpperX() - boxOrigin.getLowerX());
-	vector.y = vector.y / (boxOrigin.getUpperY() - boxOrigin.getLowerY());
-	vector.z = vector.z / (boxOrigin.getUpperZ() - boxOrigin.getLowerZ());
+void PerspectiveMath::convertBoxToBox(cVector3d& vector, const cCollisionAABBBox& boxOrigin, const cCollisionAABBBox& boxTarget) 
+{
+	// convert the vector to a unitary vector representing how far the coordinate is from the lower corner
+	vector.x = (vector.x - boxOrigin.getLowerX()) / (boxOrigin.getUpperX() - boxOrigin.getLowerX());
+	vector.y = (vector.y - boxOrigin.getLowerY()) / (boxOrigin.getUpperY() - boxOrigin.getLowerY());
+	vector.z = (vector.z - boxOrigin.getLowerZ()) / (boxOrigin.getUpperZ() - boxOrigin.getLowerZ());
 
+	// translate the vector to the relative position in the target box
 	vector.x = vector.x * (boxTarget.getUpperX() - boxTarget.getLowerX()) + boxTarget.getLowerX();
 	vector.y = vector.y * (boxTarget.getUpperY() - boxTarget.getLowerY()) + boxTarget.getLowerY();
 	vector.z = vector.z * (boxTarget.getUpperZ() - boxTarget.getLowerZ()) + boxTarget.getLowerZ();
