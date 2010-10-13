ade0902b5cb70f1e350e3a8be7ec61b9256faf4e
Kevin Mottashed <kevinmottashed@gmail.com>
2010-10-13 02:19:06 -0400

    Documented ConsoleStream and ConsoleSink classes.

--- HugMe v0.9.1/Utils/ConsoleStream.h
+++ HugMe v0.9.1/Utils/ConsoleStream.h
@@ -4,32 +4,52 @@
 #include "stdafx.h"
 #include "boost.h"
 
-// A utility for writing to the console
-// the goal of this file is to wrap the OutputDebugStr function
-// into a STL compliant string
-//
-// Usage
-// console << "some message to display" << std::endl;
-
-
-// A boost sink that writes to the debug console
+/**
+ * A boost iostream sink that writes to the debug console.
+ */
 class ConsoleSink : public boost::iostreams::sink
 {
 public:
+	/**
+	 * Write to the sink.
+	 * @param s The c style string to write.
+	 * @param n The number of characters to write.
+	 * @return The number of characters that were written
+	 */
 	std::streamsize write(const char* s, std::streamsize n);
 };
 
-// a boost stream that writes to a console sink
+/** 
+ * A utility for writing to the debug console.
+ * The goal of this class is to wrap the OutputDebugStr function into a STL compliant ostream.
+ *
+ * Sample Usage:
+ * console << "some message to display" << std::endl;
+ */
 class ConsoleStream : public boost::iostreams::stream<ConsoleSink>
 {
 public:
+	/**
+ 	 * Constructor
+	 */
 	ConsoleStream();
+
+	/**
+	 * Destructor
+	 */
 	~ConsoleStream();
 private:
+	/**
+	 * The output sink where all the output will go
+	 */
 	ConsoleSink sink;
 };
 
-// the console stream for this application
+/**
+ * The console stream for this application.
+ * This global variable is provided as a utility so that we don't need to 
+ * initialize a ConsoleStream object each time we want to output.
+ */
 static ConsoleStream console;
 
 #endif
