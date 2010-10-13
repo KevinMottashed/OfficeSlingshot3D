6d3a997e98750a98ca5b9dec34a53fc448f72314
Kevin Mottashed <kevinmottashed@gmail.com>
2010-10-13 02:24:51 -0400

    Documented the SyncLocker class.

--- HugMe v0.9.1/Utils/SyncLocker.h
+++ HugMe v0.9.1/Utils/SyncLocker.h
@@ -3,24 +3,44 @@
 
 #include "stdafx.h"
 
+/**
+ * Utility class for wrapping a mutex in the RIAA pattern.
+ * The mutex is locked on construction and released upon destruction.
+ */
 class SyncLocker
 {
-	CRITICAL_SECTION* cs;
 public:
+	/**
+	 * Constructor. Locks the mutex.
+	 * @param cs The mutex to lock.
+	 */
 	SyncLocker(CRITICAL_SECTION& cs)
 	{
 		this->cs = &cs;
 		EnterCriticalSection(this->cs);
 	}
+
+	/**
+	 * Destructor. Releases the mutex.
+	 */
 	~SyncLocker()
 	{
 		LeaveCriticalSection(this->cs);
 	}
+
+	/**
+	 * Typecast to bool operator.
+	 * Provided to aid the synchronized macro.
+	 * @return false
+	 */
 	operator bool()
 	{
 		return false;
 	}
+private:
+	CRITICAL_SECTION* cs;
 };
+
 
 #define synchronized(obj) if (SyncLocker __sync_locker__ = obj) ASSERT(0); else
 
