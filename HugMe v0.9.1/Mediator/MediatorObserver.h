7553d425da8c3078329dba57af0de2a55c9780ed
Kevin Mottashed <kevinmottashed@gmail.com>
2010-10-13 21:10:14 -0400

    Documented MediatorObserver class.

--- HugMe v0.9.1/Mediator/MediatorObserver.h
+++ HugMe v0.9.1/Mediator/MediatorObserver.h
@@ -24,6 +24,8 @@ public:
 	/**
 	 * Function that is called everytime the mediator has a notification to deliver.
 	 * This function should be used to handle the notifications.
+	 * @param context The update's context.
+	 * @param data The data associated with the update.
 	 */
 	virtual void update(MediatorUpdateContext_t context, const void* data) = 0;
 };
