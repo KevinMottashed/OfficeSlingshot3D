c449e1b1e7d613902129a0692ec3ce3fb568a50c
Kevin Mottashed <kevinmottashed@gmail.com>
2010-10-13 22:16:37 -0400

    Documented PerspectiveMath class.

--- HugMe v0.9.1/Mediator/PerspectiveMath.h
+++ HugMe v0.9.1/Mediator/PerspectiveMath.h
@@ -29,8 +29,21 @@ public:
 	 */
 	static void invert3DProjectile(Projectile& projectile);
 
+	/**
+	 * Converts the orientation of a vector from XYZ to YZX.
+	 * This effectively converts a perspective where x is depth, y is left-right and z is height to
+	 * a perspective where x is left-right, y is height and z is depth.
+	 * @param vector The vector to convert.
+	 */
 	static void convertOrientationXYZtoYZX(cVector3d& vector);
 
+	/**
+	 * Translates a point from one box to the other.
+	 * The point appears at the same relative position in the target box.
+	 * @param vector The point to translate.
+	 * @param boxOrigin The box where the point originates from.
+	 * @param boxTarget The box to which the point will be translated.
+	 */
 	static void convertBoxToBox(cVector3d& vector, const cCollisionAABBBox& boxOrigin, const cCollisionAABBBox& boxTarget);
 };
 
