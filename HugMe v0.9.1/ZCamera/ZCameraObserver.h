2ee875cdff7bab79180ed0ca9d2508c99ce9e6c4
Philippe Dore <phildore_11@hotmail.com>
2010-10-15 15:18:39 -0400

    Documented ZCameraObserver Documented ZCameraSubject

--- HugMe v0.9.1/ZCamera/ZCameraObserver.h
+++ HugMe v0.9.1/ZCamera/ZCameraObserver.h
@@ -3,21 +3,39 @@
 
 #include "stdafx.h"
 
-// each update has a context
-// the context represents what has changed in the observed object
+/**
+ * Update context enum for the zcamera.
+ * These represent the different events that a zcamera can report
+ */
 enum ZCameraUpdateContext
 {
 	VIDEO,
 	AVATAR_POSITION
 };
 
+/**
+ * Abstract class in the observer patern for the zcamera.
+ * The zcamera reports all events via the observer pattern
+ */
 class ZCameraObserver
 {
 public:
+	/**
+	 * Constructor
+	 */
 	ZCameraObserver();
+
+	/**
+	 * Destructor
+	 */
 	virtual ~ZCameraObserver();
 
-	// handles an update
+	/**
+	 * Abstract function to handle updates from the zcamera.
+	 * Every time there is an update the zcamera subject will call this method.
+	 * @param context The context of the update
+	 * @param data The data associated with the update
+	 */
 	virtual void update(ZCameraUpdateContext context, const void* data) = 0;
 };
 
--- HugMe v0.9.1/ZCamera/ZCameraSubject.h
+++ HugMe v0.9.1/ZCamera/ZCameraSubject.h
@@ -4,18 +4,39 @@
 #include "stdafx.h"
 #include "ZCameraObserver.h"
 
+/**
+ * Abstract class in the observer pattern for the zcamera.
+ * This class is used to report zcamera related updates to the observers.
+ */
 class ZCameraSubject
 {
 public:
 	ZCameraSubject();
 	virtual ~ZCameraSubject();
 
+	/**
+	 * Attach a new observer to this subject.
+	 * @param observer The observer to attach to this subject. No duplicated are allowed.
+	 */
 	void attach(ZCameraObserver* observer);
+
+	/**
+	 * Detach an observer from this subject.
+	 * @param observer The observer to detach from this subject.
+	 */
 	void detach(ZCameraObserver* observer);
 
+	/**
+	 * Notifies all observers that a new update is available.
+	 * @param context The context of the update
+	 * @param data The data associated with the update
+	 */
 	void notify(ZCameraUpdateContext context, const void* data = NULL);
 
 private:
+	/**
+	 * The list of observers currently observing this subject.
+	 */
 	std::vector<ZCameraObserver*> m_observers;
 };
 
