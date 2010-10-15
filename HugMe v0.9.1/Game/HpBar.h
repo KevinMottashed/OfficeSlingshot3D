1458ae8e8ac253ac2e631d17796410d52052a0c7
Philippe Dore <phildore_11@hotmail.com>
2010-10-15 11:45:30 -0400

    Documented the HPBar class

--- HugMe v0.9.1/Game/HpBar.h
+++ HugMe v0.9.1/Game/HpBar.h
@@ -5,17 +5,45 @@
 
 class HpBar
 {
+public:
+	/**
+	 * Constructor
+	 * @param world The world in which the HP bar will be placed
+	 * @param isLocal True is the HP bar is associated to the local user
+	 */
+	HpBar(cWorld* world, bool isLocal);
+
+	/**
+	 * Destructor
+	 */
+	~HpBar(void);
+
+	/**
+	 * Reduces the HP of the user
+	 * @ param hpLost The amount of HP to be reduced
+	 */
+	void ReduceHP(int hpLost);
+
 private:
+	/**
+	 * The current HP
+	 */
 	double hp;
+
+	/**
+	 * The default initial total health
+	 */
 	static const int totalHp = 10;
+
+	/**
+	 * The HP bar's mesh object
+	 */
 	cMesh* hpBarMesh;
 
+	/**
+	 * True if the HP bar is associated to the local user
+	 */
 	bool isLocal;
-
-public:
-	HpBar(cWorld* world, bool isLocal);
-	~HpBar(void);
-	void ReduceHP(int hpLost);
 };
 
 #endif
