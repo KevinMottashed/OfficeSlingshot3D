b8e1cc55dcc3589c97464252a51432365846042c
Kevin Mottashed <kevinmottashed@gmail.com>
2010-11-14 02:30:33 -0500

    Removed unused network error codes.

--- OfficeSlingshot3D/Network/NetworkCodes.h
+++ OfficeSlingshot3D/Network/NetworkCodes.h
@@ -15,12 +15,9 @@ enum rc_network
 	ERROR_ALREADY_CONNECTED,
 	ERROR_CONNECT_CONTROL_SOCKET,
 	ERROR_CONNECT_DATA_SOCKET,
-	ERROR_MESSAGE_QUEUE_OVERFLOW,
 	ERROR_SOCKET_ERROR,
-	ERROR_UNKNOWN_CONTROL_MESSAGE,
-	ERROR_UNKNOWN_DATA_MESSAGE,
 	ERROR_NO_CONNECTION,
-	ERROR_USER_NAME_NOT_RECEIVED
+	ERROR_USER_NAME_NOT_RECEIVED,
 };
 
 // Get the error message for a return code
@@ -46,14 +43,8 @@ static std::string lookup(rc_network code)
 		return "Error, could not connect the control socket";
 	case ERROR_CONNECT_DATA_SOCKET:
 		return "Error, could not connect the data socket";
-	case ERROR_MESSAGE_QUEUE_OVERFLOW:
-		return "Error, too many message in the queue";
 	case ERROR_SOCKET_ERROR:
 		return "Error, a socket error has occurred";
-	case ERROR_UNKNOWN_CONTROL_MESSAGE:
-		return "Error, unknown control message received";
-	case ERROR_UNKNOWN_DATA_MESSAGE:
-		return "Error, unknown data message received";
 	case ERROR_NO_CONNECTION:
 		return "Error, not connected";
 	default:
