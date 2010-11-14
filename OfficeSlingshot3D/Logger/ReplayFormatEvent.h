2a11e0f7990d6744f3f424142735e221fb704150
Kevin Mottashed <kevinmottashed@gmail.com>
2010-11-13 22:55:58 -0500

    Documented some classes.
    Documented ReplayFormatEvent and ZCameraReplayer.

--- OfficeSlingshot3D/Logger/ReplayFormatEvent.h
+++ OfficeSlingshot3D/Logger/ReplayFormatEvent.h
@@ -5,26 +5,41 @@
 #include "boost.h"
 #include "LogEventEnum.h"
 
-// The replay format event class is used to represent a replay event
-// A replay event contains a time at which the event occured where the 
-// time is the elapsed time since the application started. This class
-// also contains an enum representing the event that occured.
+/**
+ * The replay format event class is used to represent a replay event.
+ * A replay event contains a time at which the event occured where the
+ * time is the elapsed time since the application started. This class
+ * also contains an enum representing the event that occured.
+ */
 class ReplayFormatEvent
 {
 public:
+	/**< Constructor */
 	ReplayFormatEvent();
+	
+	/**
+	 * Constructor.
+	 * @param time The time at which the event occured.
+	 * @param logEvent The event that occured.
+	 */
 	ReplayFormatEvent(long time, LogEvent_t logEvent);
+	
+	/**< Destructor */
 	~ReplayFormatEvent();
-
-	long time;
-	LogEvent_t logEvent;
-
+	
+	long time; /**< The time at which the event occured. */
+	LogEvent_t logEvent; /**< The event that happened. */
+	
 private:
 	// the boost serialization library requires access to the serialize function
 	friend class boost::serialization::access;
-
-	// serialization function that the boost library will use for both
-	// serializaing and deserializing this data type
+	
+	/**
+	 * Serialization function that the boost library will use for both
+	 * serializaing and deserializing this data type.
+	 * @param ar The archive where the serialized data will be saved or read from.
+	 * @param version The version of the archive.
+	 */
 	template<class Archive>
 	void serialize(Archive & ar, const unsigned int version);
 };
--- OfficeSlingshot3D/Replay/ZCameraReplayer.h
+++ OfficeSlingshot3D/Replay/ZCameraReplayer.h
@@ -7,30 +7,47 @@
 #include "LoggerProxy.h"
 #include "ZCameraProxy.h"
 
-// The zcamera replayer class is in charge of replaying zcamera events
-// The replay function is used to replay a specific log event, at which
-// point the zcamera replayer will extract any information it needs from
-// the provided file.
+/**
+ * The zcamera replayer class is in charge of replaying zcamera events.
+ * The replay function is used to replay a specific log event, at which
+ * point the zcamera replayer will extract any information it needs from
+ * the provided file.
+ */
 class ZCameraReplayer : public IZCamera
 {
 public:
-	// contructor, takes the archive and related file that will be used to extract data
+	/**
+	 * Constructor.
+	 * @param file The file that the archive was created from.
+	 * @param archive The archive that will be read from for replaying events.
+	 */
 	ZCameraReplayer(	boost::shared_ptr<std::ifstream> file,
 						boost::shared_ptr<boost::archive::text_iarchive> archive);
+
+	/**< Destructor. */
 	virtual	~ZCameraReplayer();
 
-	// replay an event, the event must be part of the zcamera event category
+	/**
+	 * Replay an event, the event must be part of the zcamera event category.
+	 * @param logEvent The event to be replayed.
+	 */
 	void replay(LogEvent_t logEvent);
 
-	// start/stop capturing frames from the camera, part of the zcamera interface
-	// the replayer doesn't actually capture frames from a camera
+	/**
+	 * Start capturing frames from the camera.
+	 * The replayer won't actually do anything this is just to implement the camera interface.
+	 */
 	virtual void startCapture();
+
+	/**
+	 * Stop capturing frames from the camera.
+	 * The replayer won't actually do anything this is just to implement the camera interface.
+	 */
 	virtual void stopCapture();
 
 private:
-	// the file/archive used for retrieving the data associated with events
-	boost::shared_ptr<std::ifstream> file;
-	boost::shared_ptr<boost::archive::text_iarchive> archive;
+	boost::shared_ptr<std::ifstream> file; /**< File from which the archive reads data */
+	boost::shared_ptr<boost::archive::text_iarchive> archive; /**< The archive used for retriving data */
 };	
 
 #endif
